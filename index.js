a7ab4516be061dd0739a675887e4659e59a1fbee
Morphinies <90818986+Morphinies@users.noreply.github.com>
2023-06-15 14:19:07 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const form = document.getElementById("form")
+form.addEventListener("submit",addItem)
+
+function addItem(e){
+    e.preventDefault();
+    const input = document.getElementById("input");
+    const list = document.getElementById("list");
+    list += document.createElement("li")
+}
